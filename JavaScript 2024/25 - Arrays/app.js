27f1d34f476b34535bd0949e48e57fb0c81c71eb
HappyNZ95 <htewini@gmail.com>
2024-02-08 18:54:18 +1300

    JS 25 Arrays [In progress]

---
+++ JavaScript 2024/25 - Arrays/app.js
@@ -0,0 +1,15 @@
+//Array = a variable like structure that can hold more than one value at a time
+
+let fruits = ["apple", "orange", "banana", "grape", "kiwi"];
+
+fruits[4] = "coconut";
+
+fruits.push("coconut");
+
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
