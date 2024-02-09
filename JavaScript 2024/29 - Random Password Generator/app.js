2c36b0d66fe25014a4dfea82dc5cc9cbd62a5105
HappyNZ95 <htewini@gmail.com>
2024-02-09 16:55:58 +1300

    JS 29 Random Password Generator[In Progress]

---
+++ JavaScript 2024/29 - Random Password Generator/app.js
@@ -0,0 +1,17 @@
+function generatePassword(length, includeUppercase, includeNumbers, includeSymbols) {
+
+}
+
+const passwordLength = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols);
+
+console.log(`Generated password: ${password}`);
+ 
